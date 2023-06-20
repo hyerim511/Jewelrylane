07e3c8efe20d54d5444ee407af341a76372bf905
natttara <aonkaseam.n@gmail.com>
2023-06-20 02:36:34 -0700

    update footContent

--- src/components/layout/footContent.vue
+++ src/components/layout/footContent.vue
@@ -20,3 +20,24 @@ export default {
     name: 'footContent'
 }
 </script>
+
+<style scoped>
+    @import '../../assets/css/main.css';
+    footer {
+        box-shadow: 0px -5px 10px 0px rgba(0, 0, 0, 0.15);
+        position: relative;
+        width: 100%;
+        bottom: 0;
+        padding: 1%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        text-align: center;
+    }
+    aside {
+        display: flex;
+        column-gap: 5vh;
+    }
+    
+    
+</style>

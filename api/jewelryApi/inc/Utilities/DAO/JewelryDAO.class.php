cdf63ad61556225c273acbec5d80e7b05e04e502
hyerim <kimhyerim511@gmail.com>
2023-06-22 15:02:53 -0700

    delete unused lines

--- api/jewelryApi/inc/Utilities/DAO/JewelryDAO.class.php
+++ api/jewelryApi/inc/Utilities/DAO/JewelryDAO.class.php
@@ -17,26 +17,4 @@ class JewelryDAO {
         return self::$db->resultSet();
     }
 
-    public static function getJewelryById($id) {
-        $sql = "SELECT * FROM Catalog WHERE id = :id";
-  
-        self::$db->query($sql);
-        self::$db->bind(":id", $id);
-  
-        self::$db->execute();
-  
-        return self::$db->singleResult();
-    }
-  
-     public static function getCategory(string $category) {
-        $sql = "SELECT * FROM Catalog WHERE category LIKE :category";
-     
-        self::$db->query($sql);
-        self::$db->bind(":category", "%$category%");
-     
-        self::$db->execute();
-     
-        return self::$db->resultSet();
-    }
-
 }
--- api/jewelryApi/jewelry.php
+++ api/jewelryApi/jewelry.php
@@ -6,18 +6,14 @@ require_once("./inc/Utilities/PDOService.class.php");
 require_once("./inc/Utilities/JewelryConverter.class.php");
 require_once("./inc/Utilities/DAO/JewelryDAO.class.php");
 
-JewelryDAO::startDb();
-
 header("Access-Control-Allow-Origin: *");
 
 $method = $_SERVER["REQUEST_METHOD"];
 
 switch($method) {
     case "GET" :
+        JewelryDAO::startDb();
         echo json_encode(
             JewelryConverter::convertToStd(JewelryDAO::getAllJewelry())); 
     break;
-    default:
-        echo json_encode(
-            JewelryConverter::convertToStd(JewelryDAO::getAllJewelry())); 
 }

e1b43adcc82b390a995a2c432d4359cbdf12f3e9
natttara <aonkaseam.n@gmail.com>
2023-06-27 19:33:47 -0700

    test3

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import HomePage from '../components/pages/้HomePage.vue'
+import HomePage from '../components/pages/HomePage.vue'
 import AboutPage from '../components/pages/AboutPage.vue'
 import { createRouter, createWebHistory } from 'vue-router';
 
